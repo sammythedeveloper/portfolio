adabca13e1ba49950cbdf67e772a985660abfc8e
sammythedeveloper <justsammy484@gmail.com>
2025-12-31 12:00:00 -0500

    happy new year again deploy

--- src/components/providers/LenisProvider.tsx
+++ src/components/providers/LenisProvider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis } from 'lenis/react'
 
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   return <ReactLenis root>{children}</ReactLenis>;
